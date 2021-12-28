b7f8346b53a97ac1a8c4fe6e19d08cdbaa6fa240
Sidinei Silva <sidinei.silva02@gmail.com>
2021-12-28 17:49:38 -0300

    feat: :sparkles: Incluindo pagina de tema

--- lib/change_theme/change_theme_page.dart
+++ lib/change_theme/change_theme_page.dart
@@ -1,3 +1,4 @@
+import 'package:bangeek/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class ChangeThemePage extends StatefulWidget {
@@ -11,9 +12,27 @@ class _ChangeThemePageState extends State<ChangeThemePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          color: Colors.amber,
+      backgroundColor: AppTheme.colors.primaryColor,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Container(
+          child: Icon(AppTheme.icons.arrowBackLeft),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Container(
+                child: Text(
+              '1/7',
+              style: AppTheme.textStyles.stepText,
+            )),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [Container()],
         ),
       ),
     );
